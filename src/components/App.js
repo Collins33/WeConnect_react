25899eb6c53edbb571ead344d3bcf13a6f75afa7
Collins Njau <collinsnjau39@gmail.com>
2018-05-30 14:19:30 +0300

    Add method to make call to the api

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,14 @@ import Intro from './intro'
 import Business from './business'
 
 class App extends Component {
+  getBusiness = async () =>{
+    //make the api call
+    const api_call= await fetch("https://we-connect-muru.herokuapp.com/api/v2/businesses")
+    //turn response to json
+    const json_data = await api_call.json()
+    //log the data
+    console.log(json_data)
+  }
   render() {
     return (
       <div className="App">
