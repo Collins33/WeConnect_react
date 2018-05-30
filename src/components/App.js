3655d859d6766a07fa8cb904137ef61fc26ae391
Collins Njau <collinsnjau39@gmail.com>
2018-05-30 11:20:21 +0300

    Add business component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import logo from '../logo.svg';
 import '../App.css';
 import Navbar from './navbar'
 import Intro from './intro'
+import Business from './business'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Intro />
+        <Business />
       </div>
     );
   }
---
+++ src/components/business.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class Business extends Component {
+  render() {
+    return (
+        <div className="row">
+        <h2 className="text-center">
+        FEATURED BUSINESSES
+        </h2>
+        </div>
+
+    );
+  }
+}
+
+export default Business;
