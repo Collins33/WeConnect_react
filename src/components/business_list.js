4c04c756de5e7d2ecda6a0e24f01893e0d24c035
Collins Njau <collinsnjau39@gmail.com>
2018-06-13 12:17:12 +0300

    Display the business names

--- src/components/business_list.js
+++ src/components/business_list.js
@@ -3,10 +3,19 @@ import logo from '../logo.svg';
 import '../App.css';
 
 class BusinessList extends Component {
+  // method to map out the list
+  createBusiness = (item) =>{
+      return <li>{item.name}</li>
+  }
+
   render() {
+      const businessEntries = this.props.businesses;
+      const listItems=businessEntries.map(this.createBusiness)
     return (
-      <div>
-          
+      <div className="row">
+         <ul className="theList">
+               {listItems}
+            </ul>
       </div>        
 
     );
