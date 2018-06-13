aa566de7e3fd20d675b9b3df17b9546c59afd7f2
Collins Njau <collinsnjau39@gmail.com>
2018-06-13 12:55:53 +0300

    Change how all businesses are rendered

--- src/components/business_list.js
+++ src/components/business_list.js
@@ -5,7 +5,7 @@ import '../App.css';
 class BusinessList extends Component {
   // method to map out the list
   createBusiness = (item) =>{
-      return <li>{item.name}</li>
+      return <div className="col-xs-3 col-md-3 col-lg-3 panel">{item.name}</div>
   }
 
   render() {
@@ -13,9 +13,7 @@ class BusinessList extends Component {
       const listItems=businessEntries.map(this.createBusiness)
     return (
       <div className="row">
-         <ul className="theList">
-               {listItems}
-            </ul>
+        {listItems}
       </div>        
 
     );
