a4937a90f2a2576f91f2f82a2ab56f86033b8cc8
Collins Njau <collinsnjau39@gmail.com>
2018-06-13 10:22:46 +0300

    Add signup template

--- src/components/signup.js
+++ src/components/signup.js
@@ -7,10 +7,35 @@ class Signup extends Component {
     return (
         <div className="row">
         <h2 className="text-center">
-        Signup
+        
         </h2>
-        </div>
+        <div className="row form-column">
+            <div className="col-sm-12 col-md-12 col-lg-12 well">
+                <h1 className="text-center">SIGN-UP</h1>
+                <form>
+                    <div className="form-group">
+                      <input className="form-control" placeholder="enter your name" id="name-field" type="text"/>
+                    </div>
 
+                    <div className="form-group">
+                        <input className="form-control" placeholder="enter your email" id="email-field" type="email"/>
+                    </div>
+
+                    <div className="form-group">
+                        <input className="form-control" placeholder="enter password" id="password-field" type="password"/>
+                    </div>
+
+                    <div className="form-group">
+                        <input className="form-control" placeholder="confirm password" id="confirm-field" type="password"/>
+                    </div>
+
+                    <button className="btn btn-info form-button" type="submit">SIGN UP</button>
+                </form>
+                <p className="text-center">By signing up, you agree to the terms and conditions</p>
+                <p className="text-center">Already signed up? <a href="login.html"><strong>Log-in</strong></a></p>
+            </div>
+        </div>
+        </div>
     );
   }
 }
