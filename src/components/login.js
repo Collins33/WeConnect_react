c029dbc3702f81fa1dd034f938895d85ffeb72d6
Collins Njau <collinsnjau39@gmail.com>
2018-06-13 10:24:13 +0300

    Add the login template

--- src/components/login.js
+++ src/components/login.js
@@ -9,6 +9,23 @@ class Login extends Component {
         <h2 className="text-center">
        Login
         </h2>
+        <div className="row form-column">
+            <div className="col-sm-12 col-md-12 col-lg-12 well">
+                <h1 className="text-center">LOG-IN</h1>
+                <form>
+                    <div className="form-group">
+                       <input className="form-control" type="text" placeholder="enter email" id="email-login-field"/>
+                    </div>
+
+                    <div className="form-group">
+                        <input className="form-control" type="password" placeholder="enter password" id="password-login-field"/>
+                    </div>
+                    <button className="btn btn-info form-button" type="submit">Log in</button>
+                </form>
+                <p className="text-center">Don't have an account?</p>
+                <p className="text-center"><a href="registration.html"><strong>Sign-up</strong></a></p>
+            </div>
+        </div>
         </div>
 
     );
