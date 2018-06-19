cdef73af32ccaef95fc04df2eb0cae8ec276166d
Collins Njau <collinsnjau39@gmail.com>
2018-06-19 14:35:45 +0300

    Add method to make get request to the api

--- src/components/business.js
+++ src/components/business.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Business extends Component {
+  // set the default state object
+  state = {
+    business: []
+  }
+  // add the componentDidMount lifecylce method
+  componentDidMount(){
+    axios.get("https://we-connect-muru.herokuapp.com/api/v2/businesses")
+    .then(response =>{
+      // this one is executed immediately the data returns from the backend
+      //create array of businesses with the information you need
+      const newBusiness = response.data.map(business => {
+        // use map to get only the relevant data from the response 
+        return{
+          // we are taking business name and business contact from the response
+          name: business.name,
+          contact: business.contact
+        }
+      })
+
+      //create a new state without mutating the original state
+      // newState is now equal to the new object which is the newBusiness object
+      const newState = Object.assign(this.state, {businesses: newBusiness}) // new state object
+      //store the new state in the component's state
+      this.setState(newState)
+      // this sets the newState object as our new state
+     
+    })
+    .catch(error => console.log(error));
+  }
+
+
+
+
+
+
   render() {
     return (
        <div className = "row">
@@ -22,6 +57,8 @@ class Business extends Component {
             </nav>
 
        <h1 className="text-center">ALL BUSINESSES</h1>
+
+
 
        </div>
 
