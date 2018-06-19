be0abf8b6469ac2583ca13d19f9838a2dd8e585b
Collins Njau <collinsnjau39@gmail.com>
2018-06-19 15:44:47 +0300

    Add animate library

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import {Animated} from 'react-animated-css';
 import '../App.css';
 
 class Intro extends Component {
   render() {
     return (
+      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
         <div className="row intro">
-        <h2 className="text-center">
+        <h2 className="text-center bounce">
         A new way to connect with businesses around you
         </h2>
         </div>
+        </Animated>
 
     );
   }
