18001bd7346e6ac75a3028395b6f3583207a7c2f
Collins Njau <collinsnjau39@gmail.com>
2018-06-19 12:10:47 +0300

    Add comments

--- src/components/App.js
+++ src/components/App.js
@@ -21,18 +21,23 @@ class App extends Component {
   componentDidMount(){
     axios.get("https://we-connect-muru.herokuapp.com/api/v2/businesses")
     .then(response =>{
+      // this one is executed immediately the data returns from the backend
       //create array of businesses with the information you need
-      const newBusiness = response.data.map(c => {
+      const newBusiness = response.data.map(business => {
+        // use map to get only the relevant data from the response 
         return{
-          name: c.name,
-          contact: c.contact
+          // we are taking business name and business contact from the response
+          name: business.name,
+          contact: business.contact
         }
       })
 
       //create a new state without mutating the original state
-      const newState = Object.assign({}, this.state, {businesses: newBusiness})
+      // newState is now equal to the new object which is the newBusiness object
+      const newState = Object.assign(this.state, {businesses: newBusiness}) // new state object
       //store the new state in the component's state
       this.setState(newState)
+      // this sets the newState object as our new state
      
     })
     .catch(error => console.log(error));
@@ -45,6 +50,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Intro />
+         {/* bind the state to the business_list component */}
         <BusinessList businesses={this.state.businesses}/>
         <Footer />
       </div>
