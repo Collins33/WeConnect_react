15c73aceb06faf9a0c2d828c0c0c5f7ac1cccd4d
Collins Njau <collinsnjau39@gmail.com>
2018-06-20 22:51:49 +0300

    Change animation

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,7 +5,7 @@ import '../App.css';
 class Intro extends Component {
   render() {
     return (
-      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+      <Animated animationIn="zoomIn" animationOut="fadeOut" isVisible={true}>
         <div className="row intro">
         <h2 className="text-center bounce">
         A new way to connect with businesses around you
