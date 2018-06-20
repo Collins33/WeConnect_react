2bac8d64c718c77aa1b556bdd52cbda7bae8408f
Collins Njau <collinsnjau39@gmail.com>
2018-06-20 21:34:05 +0300

    Add template for reseting password

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Reset extends Component {
-  render() {
-    return (
-        <div className="row">
-         <h1 className="text-center">Reset password</h1>
-        </div>
+    state = {
 
-    );
-  }
+    }
+
+    render() {
+        return (
+            <div className="row signuprow">
+            <div className="row form-column">
+                <div className="col-sm-12 col-md-12 col-lg-12 well">
+                    <h1 className="text-center">RESET PASSWORD</h1>
+                    <form>
+                        <div className="form-group">
+                        <input className="form-control" type="text" placeholder="enter email" id="email-login-field" name="email"/>
+                        </div>
+                        <button className="btn btn-info form-button" type="submit">Reset password</button>
+                    </form>
+                    <a href = "/"><button className="btn btn-primary">Back Home</button></a>
+                </div>
+            </div>
+            </div>
+
+        );
+    }
 }
 
 export default Reset;
