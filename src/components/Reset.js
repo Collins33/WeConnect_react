19b2b64da432b3c81d291b27ed4c0634ca65c896
Collins Njau <collinsnjau39@gmail.com>
2018-06-20 21:52:58 +0300

    Add method to reset password

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import '../App.css';
+import axios from 'axios';
+import swal from 'sweetalert';
 
 class Reset extends Component {
     state = {
 
     }
-
+    
+    reset= (e) => {
+        e.preventDefault()
+        const email = e.target.elements.email.value;
+        axios.post('https://we-connect-muru.herokuapp.com/api/v2/auth/reset-password', {
+            email: email,
+        }).then(response =>{
+            console.log("password reset successfully")
+            // sweet alert pop up
+            swal({
+                title: "Success!",
+                text: "Password reset successfully",
+                icon: "success",
+                button: "Log in",
+              });
+        })
+        .catch(error => console.log(error));
+    }
     render() {
         return (
             <div className="row signuprow">
             <div className="row form-column">
                 <div className="col-sm-12 col-md-12 col-lg-12 well">
                     <h1 className="text-center">RESET PASSWORD</h1>
-                    <form>
+                    <form onSubmit={this.reset}>
                         <div className="form-group">
                         <input className="form-control" type="text" placeholder="enter email" id="email-login-field" name="email"/>
                         </div>
