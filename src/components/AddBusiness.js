5e80a94930ccaab5dc0a8e25e71c711e1782fe18
Collins Njau <collinsnjau39@gmail.com>
2018-06-25 12:08:09 +0300

    Add validation for business name

--- src/components/AddBusiness.js
+++ src/components/AddBusiness.js
@@ -46,7 +46,14 @@ class AddBusiness extends Component {
             icon: "success",
             button: "Explore",
           });
-        }).catch(error => console.log(error));
+        }).catch(error => {
+            if (error.response.status === 409){
+                swal("Error!!", "Business name already exists. Use a different name", "error");
+            }
+            else if(error.response.status === 400){
+                swal("Error!!", "Credentials are invalid. Ensure all fields are present,", "error");
+            }
+        });
     }
 
   render() {
