296a00a39bc1f200fec770dbdeb533d8bb71381c
Collins Njau <collinsnjau39@gmail.com>
2018-06-26 11:06:32 +0300

    Add sidebar with links

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -8,10 +8,19 @@ class Dashboard extends Component {
   }
   render() {
     return (
-        <div className="row">
-          <h2 className="text-center">
-          Dashboard
-          </h2>
+        <div className="row dashboardrow">
+          <div className="col-xs-3 col-md-3 col-lg-3 panel dashboardcol well">
+            <div className="row">
+             <h1 className="text-center">MyProfile</h1>
+            </div>
+            <div className="row" id="home-row">
+              <a href="/"><h1 className="text-center">Home</h1></a>
+            </div>
+          </div>
+          <div className="col-xs-9 col-md-9 col-lg-9">
+
+          </div>
+         
         </div>
 
     );
