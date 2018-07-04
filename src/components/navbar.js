ffd09d4e17bce4099d50e108607fda81ed6b99f1
Collins Njau <collinsnjau39@gmail.com>
2018-07-04 14:08:50 +0300

    Set state for the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import '../App.css';
+import axios from 'axios';
 
 class Navbar extends Component {
   state = {
-
+   authentication: false
   }
+
+  componentWillMount(){
+    const auth_token = localStorage.getItem("auth_token")
+    const config = {
+      // config with the token
+      headers: {'Authorization': "bearer " + auth_token}
+    }
+    
+  }
+
   render() {
+      if (this.state.authentication){
     return (
         <div className="row">
             <nav className="navbar navbar-inverse main-navbar">
@@ -27,6 +39,29 @@ class Navbar extends Component {
             </nav>
       </div>
     );
+}
+else{
+    return(
+        <div className="row">
+            <nav className="navbar navbar-inverse main-navbar">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand title mainTitle" href="/">WeConnect</a>
+                    </div>
+                    <ul className="nav navbar-nav">
+                    </ul>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/businesses">Businesses</a></li>
+                        <li><a href="/dashboard">Dashboard</a></li>
+                        <li><a href="/search">Search for business</a></li>
+                        
+                    </ul>
+                </div>
+            </nav>
+      </div>
+    )
+}
   }
 }
 
