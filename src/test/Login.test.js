15f4c72dcd214395431f521fdba0e60306356f42
Collins Njau <collinsnjau39@gmail.com>
2018-07-09 21:11:47 +0300

    Test the login component

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -5,5 +5,13 @@ describe('it renders the login component',()=>{
     it('contains the weconnect text',()=>{
         const weConnect = <h1 className="text-center title">WeConnect</h1>;
         expect(wrapper.contains(weConnect)).toBe(true);
+    });
+    it('contains login form', ()=>{
+        expect(wrapper.find("form").exists()).toBe(true);
+    });
+    it('contains the login button', ()=>{
+        const loginButton = <button className="btn btn-info form-button" type="submit">Log in</button>;
+        expect(wrapper.contains(loginButton)).toBe(true);
     })
+
 })
