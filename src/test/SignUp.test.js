cefa27c20d79a8c6e7c6ac1f45c93e4453285b12
Collins Njau <collinsnjau39@gmail.com>
2018-07-09 20:11:14 +0300

    Test signup component

--- src/test/SignUp.test.js
+++ src/test/SignUp.test.js
@@ -6,4 +6,10 @@ describe('it renders the signup component',()=>{
     it('contains the signup form',()=>{
         expect(wrapper.find("form").exists()).toBe(true);
     });
+    it('contains the row to hold the form',()=>{
+        expect(wrapper.find("div.row").exists()).toBe(true);
+    });
+    it('contains a button to submit the form',()=>{
+        expect(wrapper.find("button").exists()).toBe(true)
+    })
 })
