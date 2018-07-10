195cb7eec919e1cfdd007eaae0532b1f08a7c634
Collins Njau <collinsnjau39@gmail.com>
2018-07-10 11:27:54 +0300

    Test add business component

---
+++ src/test/AddBusiness.test.js
@@ -0,0 +1,9 @@
+import AddBusiness from '../components/AddBusiness';
+
+describe('it renders form to add business',()=>{
+    const wrapper  = shallow(<AddBusiness/>);
+
+    it('shows add business form',()=>{
+       expect(wrapper.find('div').length).toEqual(8)
+    });
+})
