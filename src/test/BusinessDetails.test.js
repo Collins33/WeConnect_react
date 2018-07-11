c49195be22f9caa39c508def39e965ae11a24f85
Collins Njau <collinsnjau39@gmail.com>
2018-07-11 15:57:33 +0300

    Test the search component

--- src/test/BusinessDetails.test.js
+++ src/test/BusinessDetails.test.js
@@ -1,10 +1,19 @@
 import sinon from 'sinon';
 import { shallow, mount, render } from 'enzyme';
 import BusinessDetails from '../components/businessDetails';
+import { MemoryRouter } from 'react-router-dom';
 
-describe('<BusinessDetails />', () => {
-    //  const wrapper = shallow(<BusinessDetails />);
-     it('has divs encapsulating elements', () => {
+
+describe('<BusinessDetails />', () => { 
+    //  const wrapper = shallow(
+    //     <BusinessDetails
+    //       match={{params: {id: 1}}}
+    //     />,
+    //   );
+
+     it('gets the initial state', () => {
+          //   const component = mount(<BusinessDetails />);
+          //   const instance = wrapper.find(BusinessDetails).instance;
           expect()
      });
 });
